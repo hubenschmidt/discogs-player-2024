82d7d7b6e99b90c696e0b06c7b638ac754aad5d7
William Hubenschmidt <williamroy@cumulus-erp.com>
2024-12-09 01:57:37 -0500

    add the service layer

--- app/src/controllers/discogsController.ts
+++ app/src/controllers/discogsController.ts
@@ -1,58 +1,35 @@
 import { Request, Response, NextFunction } from 'express';
-import discogsClient from '../lib/discogsClient';
+import * as discogsService from '../services/discogsService';
 
-export const getUser = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const endpoint = `/users/${req.params.username}`;
-        const user = await discogsClient(endpoint, 'get', null);
-        res.status(200).json(user.data);
-    } catch (error) {
-        console.error(`Error getting user ${req.params.username}:`, error);
-        next(error);
-    }
-};
-
-export const getCollection = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    try {
-        const folderId = 0; // Use default folder
-        const perPage = 100; // Max items per page according to Discogs API
-        const endpoint = `users/${req.params.username}/collection/folders/${folderId}/releases`;
-
-        // Fetch the first page to get total pages and initial data
-        const firstResponse = await discogsClient(`${endpoint}?page=1&per_page=${perPage}`, 'get', null);
-        const { pages: totalPages } = firstResponse.data.pagination;
-
-        // Start with the first page's releases
-        let allRecords = firstResponse.data.releases;
-
-        // Generate promises for the remaining pages
-        const pagePromises = [];
-        for (let page = 2; page <= totalPages; page++) {
-            pagePromises.push(discogsClient(`${endpoint}?page=${page}&per_page=${perPage}`, 'get', null));
-        }
-
-        // Fetch all pages in parallel
-        const responses = await Promise.all(pagePromises);
-
-        // Concatenate all releases from the responses
-        responses.forEach(({ data }) => {
-            allRecords = allRecords.concat(data.releases);
+export const getUser = (req: Request, res: Response, next: NextFunction) => {
+    discogsService
+        .getUser(req, res, next)
+        .then(user => {
+            res.status(200).json({ user });
+        })
+        .catch(err => {
+            next(err);
         });
-
-        res.status(200).json(allRecords);
-    } catch (error) {
-        console.error('Error getting record collection:', error);
-        next(error);
-    }
 };
 
-export const getRelease = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    try {
-        const endpoint = `releases/${req.params.release_id}`;
-        const release = await discogsClient(endpoint, 'get', null);
-        res.status(200).json(release.data);
-    } catch (error) {
-        console.error('Error fetching release:', error);
-        next(error);
-    }
+export const getCollection = (req: Request, res: Response, next: NextFunction) => {
+    discogsService
+        .getCollection(req, res, next)
+        .then(collection => {
+            res.status(200).json({ collection });
+        })
+        .catch(err => {
+            next(err);
+        });
+};
+
+export const getRelease = (req: Request, res: Response, next: NextFunction) => {
+    discogsService
+        .getRelease(req, res, next)
+        .then(collection => {
+            res.status(200).json({ collection });
+        })
+        .catch(err => {
+            next(err);
+        });
 };
---
+++ app/src/services/discogsService.ts
@@ -0,0 +1,58 @@
+import { Request, Response, NextFunction } from 'express';
+import discogsClient from '../lib/discogsClient';
+
+export const getUser = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const endpoint = `/users/${req.params.username}`;
+        const user = await discogsClient(endpoint, 'get', null);
+        return user;
+    } catch (error) {
+        console.error(`Error getting user ${req.params.username}:`, error);
+        next(error);
+    }
+};
+
+export const getCollection = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+        const folderId = 0; // Use default folder
+        const perPage = 100; // Max items per page according to Discogs API
+        const endpoint = `users/${req.params.username}/collection/folders/${folderId}/releases`;
+
+        // Fetch the first page to get total pages and initial data
+        const firstResponse = await discogsClient(`${endpoint}?page=1&per_page=${perPage}`, 'get', null);
+        const { pages: totalPages } = firstResponse.data.pagination;
+
+        // Start with the first page's releases
+        let collection = firstResponse.data.releases;
+
+        // Generate promises for the remaining pages
+        const pagePromises = [];
+        for (let page = 2; page <= totalPages; page++) {
+            pagePromises.push(discogsClient(`${endpoint}?page=${page}&per_page=${perPage}`, 'get', null));
+        }
+
+        // Fetch all pages in parallel
+        const responses = await Promise.all(pagePromises);
+
+        // Concatenate all releases from the responses
+        responses.forEach(({ data }) => {
+            collection = collection.concat(data.releases);
+        });
+
+        return collection;
+    } catch (error) {
+        console.error('Error getting record collection:', error);
+        next(error);
+    }
+};
+
+export const getRelease = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+        const endpoint = `releases/${req.params.release_id}`;
+        const release = await discogsClient(endpoint, 'get', null);
+        res.status(200).json(release.data);
+    } catch (error) {
+        console.error('Error fetching release:', error);
+        next(error);
+    }
+};
