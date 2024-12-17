aec7c9bbd5a7177ab93af0d1db0b34cf69c30452
William Hubenschmidt <williamroy@cumulus-erp.com>
2024-12-16 23:52:01 -0500

    optimize client build

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b2d42fb093d38d69"; }
+/******/ 		__webpack_require__.h = function() { return "843bb20aaadb0e0f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
