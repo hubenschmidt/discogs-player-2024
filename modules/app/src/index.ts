6077d625690799e72eedfe2c2948a293305628d3
William Hubenschmidt <whubenschmidt@gmail.com>
2025-10-09 06:51:14 -0400

    try to skip OIDC discovery

--- modules/app/src/index.ts
+++ modules/app/src/index.ts
@@ -29,6 +29,7 @@ const jwtCheck = auth({
     issuerBaseURL: `https://${process.env.AUTH0_DOMAIN}/`, // whubenschmidt@gmail.com Auth0 Tenant
     // issuerBaseURL: 'https://dev-i6pidxzic85x5kuy.us.auth0.com/', // locked-out Github-Auth0 Tenant
     tokenSigningAlg: 'RS256',
+    jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`, // <- skip OIDC discovery
 });
 app.use(jwtCheck);
 
