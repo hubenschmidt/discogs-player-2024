0a1cd0be72bb22303a67dc22b86b8beb2ea03cb7
William Hubenschmidt <whubenschmidt@gmail.com>
2025-10-09 02:37:14 -0400

    deploy

--- modules/app/src/models/index.ts
+++ modules/app/src/models/index.ts
@@ -2,27 +2,48 @@
 
 const fs = require('fs');
 const path = require('path');
-const Sequelize = require('sequelize');
+const { Sequelize, DataTypes } = require('sequelize');
+
 const basename = path.basename(__filename);
-const config = require('../conf/db.js');
+
+// Load the env-specific config from conf/db.js
+const env = process.env.NODE_ENV || 'development';
+const allConfigs = require('../conf/db.js');
+const dbConfig = allConfigs[env];
+
+if (!dbConfig) {
+    throw new Error(`Sequelize config missing for env "${env}" in conf/db.js`);
+}
+
+let sequelize: any;
+
+// If using DATABASE_URL (use_env_variable), pass the URL + options
+if (dbConfig.use_env_variable) {
+    const url = process.env[dbConfig.use_env_variable];
+
+    sequelize = new Sequelize(url, dbConfig);
+} else {
+    // Otherwise pass discrete fields (must include dialect)
+    sequelize = new Sequelize(
+        dbConfig.database,
+        dbConfig.username,
+        dbConfig.password,
+        dbConfig,
+    );
+}
 
 const db: { [key: string]: any } = {};
-const sequelize = new Sequelize(config.database, config.username, config.password, {
-    ...config,
-});
 
 fs.readdirSync(__dirname)
-    .filter((file: string) => {
-        return (
+    .filter(
+        (file: string) =>
             file.indexOf('.') !== 0 &&
             file !== basename &&
             (file.endsWith('.ts') || file.endsWith('.js')) &&
-            !file.endsWith('.test.ts')
-        );
-    })
+            !file.endsWith('.test.ts'),
+    )
     .forEach((file: string) => {
-        // Use require directly for `module.exports`
-        const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+        const model = require(path.join(__dirname, file))(sequelize, DataTypes);
         db[model.name] = model;
     });
 
