7a1f03a40fdf4b7a337ba3568b613497e6f1c3e5
William Hubenschmidt <whubenschmidt@gmail.com>
2025-10-09 01:55:01 -0400

    add port to conf db

--- modules/app/src/conf/db.js
+++ modules/app/src/conf/db.js
@@ -5,6 +5,7 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
     dialect: 'postgres',
     logging: false,
     migrationStorageTableName: 'SequelizeMeta_core',
